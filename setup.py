045c13a879f03d0fc5d128673db7269451b998c4
Boris Raicheff <b@raicheff.com>
2016-11-26 16:49:16 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flask-filerev',
-    version='0.1.0',
+    version='0.1.1',
     description='Flask-FileRev',
     author='Boris Raicheff',
     author_email='b@raicheff.com',

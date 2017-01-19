0314b2da34835e91e7b09459538a49e73de75dcc
Boris Raicheff <b@raicheff.com>
2017-01-19 19:21:26 +0000

    Reset version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Flask-FileRev',
-    version='0.1.1',
+    version='0.1.0',
     description='Flask-FileRev',
     author='Boris Raicheff',
     author_email='b@raicheff.com',

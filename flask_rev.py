4b0876474152227840c7320e236a668e56978275
Boris Raicheff <b@raicheff.com>
2019-01-16 13:15:59 +0000

    Refactor implementation

--- flask_rev.py
+++ flask_rev.py
@@ -6,21 +6,11 @@
 #
 
 
-"""
-https://thusoy.com/2014/server-side-assets-file-revisioning
-https://github.com/yeoman/grunt-filerev
-https://github.com/richardbolt/grunt-filerev-assets
-"""
-
-
 import json
-import warnings
-
-from flask import url_for, current_app
-from werkzeug.local import LocalProxy
+import logging
 
 
-assets = LocalProxy(lambda: current_app.config.get('FILEREV'))
+logger = logging.getLogger('Flask-Rev')
 
 
 class Rev(object):
@@ -32,47 +22,32 @@ class Rev(object):
     :param app: Flask app to initialize with. Defaults to `None`
     """
 
+    # https://thusoy.com/2014/server-side-assets-file-revisioning
+
+    manifest = None
+
     def __init__(self, app=None):
-        self.app = app
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
-        # app.context_processor(revved_url_for)
-        global url_for
-        url_for = app.jinja_env.globals['url_for']
-
-        app.jinja_env.globals['url_for'] = _revved_url_for
-        app.filerev = self
-
-        def inject_assets():
-            return dict(assets=app.config.get('FILEREV'))
-
-        app.context_processor(inject_assets)
-
-        # TODO
+        manifest = app.config.get('REV_MANIFEST')
+        if manifest is None:
+            logger.debug('REV_MANIFEST not set')
+            return
         try:
-            with app.open_resource(app.config.get('REV_MANIFEST'), mode='r') as f:
-                filerevs = json.load(f)
-                app.config['FILEREV'] = filerevs
-        except IOError:
-            warnings.warn('REV_MANIFEST not set', RuntimeWarning, stacklevel=2)
-            app.config['FILEREV'] = {}
+            with app.open_resource(manifest, mode='r') as fp:
+                self.manifest = json.load(fp)
+        except IOError as error:
+            logger.error(str(error))
+            return
+        app.url_defaults(self._revisioned_filename)
 
-
-def revved_url_for():
-    return {'url_for': _revved_url_for}
-
-
-def _revved_url_for(endpoint, **values):
-    if endpoint == 'static':
-        original_filename = values.get('filename')
-        if original_filename:
-            revved_filename = current_app.config['FILEREV'].get(original_filename)
-            if revved_filename:
-                del values['filename']
-                return url_for(endpoint, filename=revved_filename, **values)
-    return url_for(endpoint, **values)
+    def _revisioned_filename(self, endpoint, values):
+        if endpoint == 'static':
+            filename = self.manifest.get(values.get('filename'))
+            if filename is not None:
+                values['filename'] = filename
 
 
 # EOF
